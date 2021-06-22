09d777533ef506bc0c88d9b6c2c8835c6ec70068
zph <2035143418@qq.com>
2021-06-22 10:12:23 +0800

    git-test master version7.0

--- src/main/java/com/zph/utils/UtilsTest.java
+++ src/main/java/com/zph/utils/UtilsTest.java
@@ -1,7 +1,8 @@
 package com.zph.utils;
 
 public class UtilsTest {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("change demo in github server!");
+        System.out.println("test gitee");
     }
 }
