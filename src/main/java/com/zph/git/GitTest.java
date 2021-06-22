f6183006e6739d9cd7d5a83f4b2b83d6efc7b446
zph <2035143418@qq.com>
2021-06-22 09:29:32 +0800

    git-test master version6.0

--- src/main/java/com/zph/git/GitTest.java
+++ src/main/java/com/zph/git/GitTest.java
@@ -2,11 +2,7 @@ package com.zph.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello git！");
-        System.out.println("Hello git！");
-        System.out.println("Hello git！");
-        System.out.println("Hello git！");
-        System.out.println("Hello git！");
+        System.out.println("change demo");
         System.out.println("master test！");
         System.out.println("hot-fix test！");
     }
